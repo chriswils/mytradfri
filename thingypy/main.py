98158e18878c74c96d8c3ba3772705230c6ccc7d
Christian Wilhelmsen <christian.wilhelmsen87@gmail.com>
2017-11-03 22:44:30 +0100

    added main.py with example code without scanning

--- thingypy/main.py
+++ thingypy/main.py
@@ -1,1 +1,43 @@
 # Thingy 52 HomeAssistant component
+# My thingy mac: D0:C0:1D:B9:45:E1
+
+
+from bluepy import btle, thingy52
+import binascii
+
+MAC_ADDRESS = "d0:c0:1d:b9:45:e1"
+
+print("# Creating new delegate class to handle notifications...")
+class NewDelegate(btle.DefaultDelegate):
+    def handleNotification(self, hnd, data):
+        if (hnd == thingy52.e_temperature_handle):
+            print("Notification: Temperature received: {}".format(repr(data)))
+        if (hnd == thingy52.ui_button_handle):
+            print("Notification: Button press received: {}".format(repr(data)))
+
+print("# Connecting to Thingy with address {}...".format(MAC_ADDRESS))
+thingy = thingy52.Thingy52(MAC_ADDRESS)
+
+#print("# Setting notification handler to default handler...")
+#thingy.setDelegate(thingy52.MyDelegate())
+print("# Setting notification handler to new handler...")
+thingy.setDelegate(NewDelegate())
+
+print("# Configuring and enabling temperature notification...")
+thingy.environment.enable()
+thingy.environment.configure(temp_int=1000)
+thingy.environment.set_temperature_notification(True)
+
+print("# Configuring and enabling button press notification...")
+thingy.ui.enable()
+thingy.ui.set_btn_notification(True)
+
+print("# Waiting for three notifications...")
+thingy.waitForNotifications(timeout=5)
+thingy.waitForNotifications(timeout=5)
+thingy.waitForNotifications(timeout=5)
+
+print("# Disconnecting...")
+thingy.disconnect()
+
+
