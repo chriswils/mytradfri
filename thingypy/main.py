60394ad79ae8f7f3ddb6e17a627e8c272c5f829f
Christian Wilhelmsen <christian.wilhelmsen87@gmail.com>
2017-11-04 00:43:55 +0100

    updated main.py for testing stuffs

--- thingypy/main.py
+++ thingypy/main.py
@@ -8,13 +8,19 @@ import binascii
 MAC_ADDRESS = "d0:c0:1d:b9:45:e1"
 
 print("# Creating new delegate class to handle notifications...")
-class NewDelegate(btle.DefaultDelegate):
+class NotificationDelegate(btle.DefaultDelegate):
+    def __init__(self, obj):
+        self.thingyobj = obj
+
     def handleNotification(self, hnd, data):
         if (hnd == thingy52.e_temperature_handle):
             print("Notification: Temperature received: {}".format(repr(data)))
             teptep = binascii.b2a_hex(data)
             print('Notification: Temp received:  {}.{} degCelcius'.format(
                         self._str_to_int(teptep[:-2]), int(teptep[-2:], 16)))
+            
+            self.thingyobj._state = self._str_to_int(teptep[:-2]), int(teptep[-2:], 16))
+
         if (hnd == thingy52.ui_button_handle):
             print("Notification: Button press received: {}".format(repr(data)))
     
@@ -25,29 +31,44 @@ class NewDelegate(btle.DefaultDelegate):
             i -= 2**8
         return i 
 
-print("# Connecting to Thingy with address {}...".format(MAC_ADDRESS))
-thingy = thingy52.Thingy52(MAC_ADDRESS)
+class StartAndTest():
+    def __init__():
+        self._state = None
 
-#print("# Setting notification handler to default handler...")
-#thingy.setDelegate(thingy52.MyDelegate())
-print("# Setting notification handler to new handler...")
-thingy.setDelegate(NewDelegate())
+        print("# Connecting to Thingy with address {}...".format(MAC_ADDRESS))
+        thingy = thingy52.Thingy52(MAC_ADDRESS)
 
-print("# Configuring and enabling temperature notification...")
-thingy.environment.enable()
-thingy.environment.configure(temp_int=1000)
-thingy.environment.set_temperature_notification(True)
+        #print("# Setting notification handler to default handler...")
+        #thingy.setDelegate(thingy52.MyDelegate())
+        print("# Setting notification handler to new handler...")
 
-print("# Configuring and enabling button press notification...")
-thingy.ui.enable()
-thingy.ui.set_btn_notification(True)
+        thingy.setDelegate(NotificationDelegate(), self)
 
-print("# Waiting for three notifications...")
-thingy.waitForNotifications(timeout=5)
-thingy.waitForNotifications(timeout=5)
-thingy.waitForNotifications(timeout=5)
+        print("# Configuring and enabling temperature notification...")
+        thingy.environment.enable()
+        thingy.environment.configure(temp_int=1000)
+        thingy.environment.set_temperature_notification(True)
 
-print("# Disconnecting...")
-thingy.disconnect()
+        print("# Configuring and enabling button press notification...")
+        thingy.ui.enable()
+        thingy.ui.set_btn_notification(True)
+
+    def waitfordata(self):
+        print("# Waiting for three notifications...")
+        thingy.waitForNotifications(timeout=5)
+        print("Internal state")
+        print self._state
+        thingy.waitForNotifications(timeout=5)
+        print("Internal state")
+        print self._state
+        thingy.waitForNotifications(timeout=5)
+        print("Internal state")
+        print self._state
+    
+    def disconnect(self):
+        print("# Disconnecting...")
+        thingy.disconnect()
 
 
+if (__name__ is "__main__"):
+    t = StartAndTest()
